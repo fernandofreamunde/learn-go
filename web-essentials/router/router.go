f834c1818963368ad47a9133d66be39fa9d2275a
Fernando Andrade <fandrade@bunq.com>
2019-09-03 11:06:08 +0200

    :pencil: add notes with more methods of the router

--- web-essentials/router/router.go
+++ web-essentials/router/router.go
@@ -22,3 +22,30 @@ func main() {
 
 	http.ListenAndServe(":81", router)
 }
+
+// ### Features of the gorilla/mux Router
+// # Methods
+// Restrict the request handler to specific HTTP methods.
+
+// r.HandleFunc("/books/{title}", CreateBook).Methods("POST")
+// r.HandleFunc("/books/{title}", ReadBook).Methods("GET")
+// r.HandleFunc("/books/{title}", UpdateBook).Methods("PUT")
+// r.HandleFunc("/books/{title}", DeleteBook).Methods("DELETE")
+
+// # Hostnames & Subdomains
+// Restrict the request handler to specific hostnames or subdomains.
+
+// r.HandleFunc("/books/{title}", BookHandler).Host("www.mybookstore.com")
+
+// # chemes
+// Restrict the request handler to http/https.
+
+// r.HandleFunc("/secure", SecureHandler).Schemes("https")
+// r.HandleFunc("/insecure", InsecureHandler).Schemes("http")
+
+// # Path Prefixes & Subrouters
+// Restrict the request handler to specific path prefixes.
+
+// bookrouter := r.PathPrefix("/books").Subrouter()
+// bookrouter.HandleFunc("/", AllBooks)
+// bookrouter.HandleFunc("/{title}", GetBook)
