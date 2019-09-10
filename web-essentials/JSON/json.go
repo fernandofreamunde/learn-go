08ffea41efbadb700ece04a83876ccd68a94a2b9
Fernando Andrade <fandrade@bunq.com>
2019-09-10 11:31:37 +0200

    :bug: fix json.go code

--- web-essentials/JSON/json.go
+++ web-essentials/JSON/json.go
@@ -7,30 +7,27 @@ import (
 )
 
 type User struct {
-	firstName string `json:"first_name"`
-	lastName  string `json:"last_name"`
-	age 	  int 	 `json:"age"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	Age 	  int 	 `json:"age"`
 }
 
 //  OK this is not really working for me, it gives me empty json/struct 
 // so encoding and decoding is not working as expected, will have to investigate later...
+// Turns out I need to capitalize the vars in the struct for some reason
 func main() {
 	http.HandleFunc("/decode", func(w http.ResponseWriter, r *http.Request) {
 		var user User
-		err := json.NewDecoder(r.Body).Decode(&user)
-		if err != nil {
-			http.Error(w, err.Error(), 400)
-			return
-		}
-		fmt.Println(user);
-		fmt.Fprintf(w, "\n%s %s is %d years old!\n\n", user.firstName, user.lastName, user.age)
+		json.NewDecoder(r.Body).Decode(&user)
+
+		fmt.Fprintf(w, "\n%s %s is %d years old!\n\n", user.FirstName, user.LastName, user.Age)
 	})
 
 	http.HandleFunc("/encode", func (w http.ResponseWriter, r *http.Request) {
 		peter := User {
-			firstName: "Peter",
-			lastName:  "Parker",
-			age:       15,
+			FirstName: "Peter",
+			LastName:  "Parker",
+			Age:       15,
 		}
 
 		fmt.Println(peter);
