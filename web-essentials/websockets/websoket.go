301c7c0c4113f47d17ddbfcaf9b7270c944ac21f
Fernando Andrade <fandrade@bunq.com>
2019-09-11 12:02:33 +0200

    :alembic: my first time with web sockets :P

---
+++ web-essentials/websockets/websoket.go
@@ -0,0 +1,42 @@
+package main
+
+// go get github.com/gorilla/websocket
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/websocket"
+)
+
+var upgrader = websocket.Upgrader {
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+}
+
+func main() {
+	http.HandleFunc("/echo", func(w http.ResponseWriter, r *http.Request) {
+		conn, _ := upgrader.Upgrade(w, r, nil) // error ignored for simplicity
+
+		for {
+			// Read message from browser
+			msgType, msg, err := conn.ReadMessage()
+			if err != nil {
+				return
+			}
+
+			// Print the message to the console
+			fmt.Printf("%s sent: %s\n", conn.RemoteAddr(), string(msg))
+
+			// Write message nack to the browser
+			if err = conn.WriteMessage(msgType, msg); err != nil {
+				return
+			}
+		}
+	})
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		http.ServeFile(w, r, "websocket.html")
+	})
+
+	http.ListenAndServe(":81", nil)
+}
